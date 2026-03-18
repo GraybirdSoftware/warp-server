5322d7b715b0f5afe5588b9330db8fb1c64654fd
vablings <47798035+vablings@users.noreply.github.com>
2026-03-18 18:57:41 -0500

    move server startup to own file for unit testing

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,21 @@
-use actix_web::{App, HttpServer, web};
+use std::net::TcpListener;
 
-use crate::handlers::{
-    auth, commits, files, functions, search, sources, status, symbols, targets, types, users,
-};
 
-mod database;
-mod handlers;
-mod models;
-mod routes;
-
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let pool = database::create_pool().await;
 
-    HttpServer::new(move || {
-        App::new().app_data(pool.clone()).service(
-            web::scope("/api").service(
-                web::scope("/v1")
-                    .service(
-                        web::scope("auth")
-                            .service(auth::login)
-                            .service(auth::logout)
-                            .service(auth::get_callback),
-                    )
-                    .service(
-                        web::scope("commits")
-                            .service(commits::query)
-                            .service(commits::get_commit)
-                            .service(commits::delete_commit),
-                    )
-                    .service(
-                        web::scope("files")
-                            .service(files::files)
-                            .service(files::json),
-                    )
-                    .service(
-                        web::scope("functions")
-                            .service(functions::query)
-                            .service(functions::query_source)
-                            .service(functions::get_function)
-                            .service(functions::get_function_comments)
-                            .service(functions::get_function_constraints)
-                            .service(functions::get_function_data)
-                            .service(functions::get_function_symbol)
-                            .service(functions::get_function_type),
-                    )
-                    .service(web::scope("search").service(search::search))
-                    .service(
-                        web::scope("sources")
-                            .service(sources::create_source)
-                            .service(sources::query)
-                            .service(sources::get_source)
-                            .service(sources::update_source)
-                            .service(sources::get_source_tags)
-                            .service(sources::get_source_users),
-                    )
-                    .service(web::scope("status").service(status::get_status))
-                    .service(
-                        web::scope("symbols")
-                            .service(symbols::query)
-                            .service(symbols::get_symbol),
-                    )
-                    .service(
-                        web::scope("targets")
-                            .service(targets::query)
-                            .service(targets::get_target),
-                    )
-                    .service(
-                        web::scope("types")
-                            .service(types::query)
-                            .service(types::get_types)
-                            .service(types::get_types_data),
-                    )
-                    .service(
-                        web::scope("users")
-                            .service(
-                                web::scope("me")
-                                    .service(users::me::get_user)
-                                    .service(users::me::get_keys)
-                                    .service(users::me::create_key),
-                            )
-                            .service(users::query)
-                            .service(users::get_user)
-                            .service(users::create_user)
-                            .service(users::delete_user)
-                            .service(users::get_keys)
-                            .service(users::create_key)
-                            .service(users::change_role)
-                            .service(users::change_username),
-                    ),
-            ),
-        )
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let pool = sqlx::sqlite::SqlitePool::connect("sqlite://sqlite.db?mode=rwc")
+        .await
+        .unwrap();
+
+    sqlx::migrate!("./migrations").run(&pool).await.unwrap();
+
+    let port = 80;
+    let address = format!("127.0.0.1:{}", port);
+    let listener = TcpListener::bind(address)?;
+
+    warp_server::startup::run(listener, pool)?.await?;
+
+    Ok(())
+    
 }
---
+++ src/startup.rs
@@ -0,0 +1,91 @@
+use std::net::TcpListener;
+use actix_web::{App, HttpServer, dev::Server, web};
+use sqlx::{Pool, Sqlite};
+use crate::handlers::{
+    auth, commits, files, functions, search, sources, status, symbols, targets, types, users,
+};
+
+pub fn run(listener: TcpListener, pool: Pool<Sqlite>) -> Result<Server, std::io::Error> { 
+    let server = HttpServer::new(move || {
+        App::new().app_data(pool.clone()).service(
+            web::scope("/api").service(
+                web::scope("/v1")
+                    .service(
+                        web::scope("auth")
+                            .service(auth::login)
+                            .service(auth::logout)
+                            .service(auth::get_callback),
+                    )
+                    .service(
+                        web::scope("commits")
+                            .service(commits::query)
+                            .service(commits::get_commit)
+                            .service(commits::delete_commit),
+                    )
+                    .service(
+                        web::scope("files")
+                            .service(files::files)
+                            .service(files::json),
+                    )
+                    .service(
+                        web::scope("functions")
+                            .service(functions::query)
+                            .service(functions::query_source)
+                            .service(functions::get_function)
+                            .service(functions::get_function_comments)
+                            .service(functions::get_function_constraints)
+                            .service(functions::get_function_data)
+                            .service(functions::get_function_symbol)
+                            .service(functions::get_function_type),
+                    )
+                    .service(web::scope("search").service(search::search))
+                    .service(
+                        web::scope("sources")
+                            .service(sources::create_source)
+                            .service(sources::query)
+                            .service(sources::get_source)
+                            .service(sources::update_source)
+                            .service(sources::get_source_tags)
+                            .service(sources::get_source_users),
+                    )
+                    .service(web::scope("status").service(status::get_status))
+                    .service(
+                        web::scope("symbols")
+                            .service(symbols::query)
+                            .service(symbols::get_symbol),
+                    )
+                    .service(
+                        web::scope("targets")
+                            .service(targets::query)
+                            .service(targets::get_target),
+                    )
+                    .service(
+                        web::scope("types")
+                            .service(types::query)
+                            .service(types::get_types)
+                            .service(types::get_types_data),
+                    )
+                    .service(
+                        web::scope("users")
+                            .service(
+                                web::scope("me")
+                                    .service(users::me::get_user)
+                                    .service(users::me::get_keys)
+                                    .service(users::me::create_key),
+                            )
+                            .service(users::query)
+                            .service(users::get_user)
+                            .service(users::create_user)
+                            .service(users::delete_user)
+                            .service(users::get_keys)
+                            .service(users::create_key)
+                            .service(users::change_role)
+                            .service(users::change_username),
+                    ),
+            ),
+        )
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
