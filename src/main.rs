3196d1d5bc3f7589cb95ee1087d5486278b4bc9f
vablings <47798035+vablings@users.noreply.github.com>
2026-03-19 22:35:54 -0500

    add tracing subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
 
     color_eyre::install().unwrap();
 
+    let subscriber = get_subscriber(
+        "debug".to_string(),  // default log level
+        std::io::stdout,     // log to stdout
+    );
+    
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to set subscriber");
+    
     let pool = sqlx::sqlite::SqlitePool::connect("sqlite://sqlite.db?mode=rwc")
         .await
         .unwrap();
@@ -18,4 +27,24 @@ async fn main() -> std::io::Result<()> {
 
     Ok(())
     
+}
+
+use actix_web::web;
+use tracing::Subscriber;
+use tracing_subscriber::{EnvFilter, fmt::MakeWriter};
+
+pub fn get_subscriber<Sink>(
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Sync + Send
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(env_filter));
+    
+    tracing_subscriber::fmt()
+        .with_writer(sink)
+        .with_env_filter(env_filter)
+        .finish()
 }
